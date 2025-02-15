bc3d3e79ed5519133052c15cff5600471589ce26
LUkas Nuth <lukas@familie-nuth.de>
2025-02-15 12:58:59 +0100

    add ability to push collding objects

--- player/player.gd
+++ player/player.gd
@@ -3,12 +3,14 @@ class_name Player extends CharacterBody2D
 var direction: Vector2 = Vector2.ZERO
 
 @export var speed: int = 300
+@export var push_force: float = 10.0
+var min_push_force:float = 5.0
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	pass
 
-func _process(_delta):
+func _physics_process(delta):
 	direction = Vector2(
 		Input.get_axis("ui_left", "ui_right"),
 		Input.get_axis("ui_up", "ui_down")
@@ -16,6 +18,12 @@ func _process(_delta):
 	direction.normalized()
 	velocity = direction * speed
 	move_and_slide()
+	for index in get_slide_collision_count():
+		var collision = get_slide_collision(index)
+		if collision.get_collider() is RigidBody2D:
+			var push_force = (push_force * velocity.length() / speed) + min_push_force
+			collision.get_collider().apply_central_impulse(-collision.get_normal() * push_force)
+	
 	pass
 	
 	
