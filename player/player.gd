2b3f5ca9e7d92e340c4fd88522a7f082c395bd38
LUkas Nuth <lukas@familie-nuth.de>
2025-02-16 13:56:32 +0100

    player alternative movement v3

--- player/player.gd
+++ player/player.gd
@@ -5,7 +5,7 @@ var direction: Vector2 = Vector2.ZERO
 @export var speed: int = 300
 @export var push_force: float = 10
 
-var friction = 0.18
+var rotation_speed = 1.5  # turning speed in radians/sec
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -13,42 +13,45 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	pass
 
-#
-#func _physics_process(delta):
-	#direction = Vector2(
-		#Input.get_axis("ui_left", "ui_right"),
-		#Input.get_axis("ui_down", "ui_up")
-	#)
-	#
-	#
-	#direction.normalized()
-	#velocity = direction * speed
-	#for index in get_slide_collision_count():
-		#var collision = get_slide_collision(index)
-		#var collison_body = collision.get_collider()
-		#if collison_body is RigidBody2D:			
-			#collison_body.apply_central_impulse(-collision.get_normal() * push_force)
-	#
-	#move_and_slide()
-	#pass
-	
 func _physics_process(delta):
+	# _movement_v1()
+	# _movement_v2()
+	_movement_v3(delta)
+	pass
+		
+func _movement_v1():
+	direction = Vector2(
+		Input.get_axis("ui_left", "ui_right"),
+		Input.get_axis("ui_down", "ui_up")
+	)
+	direction.normalized()
+	velocity = direction * speed
+	handle_collision()
+	look_at(get_global_mouse_position())
+	move_and_slide()
+
+func _movement_v2():
 	look_at(get_global_mouse_position())
 	var move_input = Input.get_axis("ui_down", "ui_up")
 	velocity = transform.x * move_input * speed
+	handle_collision()
 	move_and_slide()
+
+
+func _movement_v3(delta):
+
+	var move_input = Input.get_axis("ui_down", "ui_up")
+	velocity = transform.x * move_input * speed
+	var rotation_direction = Input.get_axis("ui_left", "ui_right")
+	rotation += rotation_direction * rotation_speed * delta
+	direction.normalized()
+	velocity = transform.x * move_input * speed
+	handle_collision()
+	move_and_slide()
+
+func handle_collision():
 	for index in get_slide_collision_count():
 		var collision = get_slide_collision(index)
 		var collison_body = collision.get_collider()
 		if collison_body is RigidBody2D:			
 			collison_body.apply_central_impulse(-collision.get_normal() * push_force)
-	pass
-	
-func _input(event):
-	if event is InputEventMouseMotion:
-		look_at(get_global_mouse_position())
-	elif  event is InputEventJoypadMotion:
-		var joystick_horizontal = Input.get_joy_axis(0, JOY_AXIS_RIGHT_X)
-		var joystick_vertical = Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y)
-		rotation = atan2(joystick_vertical, joystick_horizontal)
-	pass
